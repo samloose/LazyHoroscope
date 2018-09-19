e58da6fad60ab4e119b5de1b22c335a763b69d7a
sam.loose <sloose@jaguar.csuchico.edu>
2018-09-19 16:42:36 -0700

    Finished project

--- main.cpp
+++ main.cpp
@@ -8,13 +8,22 @@ int main()
 {
     string first;
     string last;
+    int lucky;
     
     cout <<"What is your first name?"<<endl;
     cin >> first;
     cout <<"What is your last name?"<<endl;
     cin >> last;
-
-  //tell fortune
-
+   
+    cout <<"Welcome,";
+    string word = first;
+    cout<<word[0];
+    word = last; 
+    cout<<word[0];
+    cout<<", here is your fortune..."<<endl;
+    
+    lucky = first.length();
+    cout<<"Your lucky number is "<<first.length()<<" letters"<<endl;
+    
   return 0;
 }
